00a86851f8bc3aaa79569946496739099acf5ba8
zhuhui <zhuhui@lonlife.cn>
2016-12-28 11:04:53 +0800

    webpack

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,11 +1,21 @@
+const path=require('path');
+const fs=require('fs');
+
+const entry_path=path.resolve(__dirname,'./public/src/js/src/entry/');
+let entry_files={};
+fs.readdirSync(entry_path).filter(function(file){
+    return file.indexOf('.js')!==-1;
+}).forEach(function(file){
+    let result=/^(.+)\.js$/.exec(file);
+    entry_files[result[1]?result[1]:file]=path.resolve(__dirname,'./public/js/src/entry/'+file);
+})
 
 var configs={
-    entry:{
-        'index':'./public/js/src/entry/index.js'
-    },
+    entry:entry_files,
     output:{
         path:__dirname+'./',
-        filename:'[name].js'
+        filename:'[name].js',
+        sourceMapFilename:'[file].map'
     }
 }
 module.exports = configs;
